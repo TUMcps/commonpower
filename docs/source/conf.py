53888017d0dd2abf42873b36458005b1fdeb4a97
Michael Eichelbeck <michael.eichelbeck@tum.de>
2024-06-14 18:30:58 +0200

    improved tutorial doc generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath(".."))  # Source code dir relative to this fi
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "CommonPower"
-copyright = "2023, TUM CPS"
+copyright = "2024, TUM CPS"
 author = "Michael Eichelbeck, Hannah Markgraf"
 release = "1.0"
 
@@ -30,7 +30,6 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
-    "sphinx_autodoc_typehints",
     "sphinx.ext.viewcode",
     "nbsphinx",
     "sphinx_rtd_theme",
